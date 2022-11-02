2a8604103ee928b366acb03702d078569bed6552
William Luck <luck.william.h@gmail.com>
2022-11-02 16:04:22 -0300

    commit tree c4e89ee0729ef1acf896ced93c88ac378082f9a8

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,20 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    # only flag takes an array of symbols
+    # except works in reverse
+    # Can include methods which will be sent as part of the response
+    render json: cheeses, except: [:created_at, :updated_at], methods: :summary
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese 
+      render json: cheese
+    else
+      render json: { error: 'Cheese not found'}, status: :not_found
+    end
   end
 
 end
